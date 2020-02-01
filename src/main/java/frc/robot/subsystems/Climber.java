073435923164f80d4f2960f18cc172383e3bafaa
Andrew Barton <abars@barleyfarm.com>
2020-02-01 11:38:11 -0600

    Set up winch motor and created pseudocode
    wrote pseudocode for an encoder once it is set up

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,25 +8,47 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-public class Climber extends CommandBase {
+public class Climber extends SubsystemBase {
+
+  private final Talon m_winchMotor;
 
   Solenoid climberSolenoid = new Solenoid(0);
-
   /**
    * Creates a new climberSubsystem
    */
   public Climber() {
 
+    m_winchMotor = new Talon(Constants.kWinchMotor);
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
+  //targetDist should not be max possible distance to account for sample time
+  public void climb(double speed, double targetDist){
+    boolean end = false;
 
- 
+    m_winchMotor.setSpeed(speed);
+
+    /* 
+    for(end != true){
+      if(targetDist >= encoderReading){
+        sleep(50);
+      }else if(targetDist < encoderReading){
+        end == true;
+      }
+    }
+    */
+
+    m_winchMotor.stopMotor();
+
+  }
  
   // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+ // @Override
+  //public boolean isFinished() {
+   // return false;
+ // }
 }
