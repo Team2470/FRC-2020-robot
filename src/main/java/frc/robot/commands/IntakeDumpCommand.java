3bff6fbe0e140d10abdab3235be47a9ac3891d0a
Ryan Sjostrand <ryansjostrand@gmail.com>
2020-03-11 22:15:52 -0500

    When dumping balls lower the intake

--- src/main/java/frc/robot/commands/IntakeDumpCommand.java
+++ src/main/java/frc/robot/commands/IntakeDumpCommand.java
@@ -32,6 +32,7 @@ public class IntakeDumpCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_Intake.deployIntake();
     m_Intake.startIntakeMotor(-0.5);
   }
 
